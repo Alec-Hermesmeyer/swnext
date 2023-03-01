57dd66623faf142a9720fc72d7be9a6eef41e148
Alec-Hermesmeyer <ahrmsmyr@gmail.com>
2023-03-01 14:28:05 -0600

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout'
+import { Analytics } from '@vercel/analytics/react'
 import '@/styles/globals.css'
 
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
