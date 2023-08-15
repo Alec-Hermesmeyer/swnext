6daff21549d87c42e8d18f59a0cd3adbe4729573
Alec Hermessmeyer <alechermesmeyer@Alecs-MBP.lan>
2023-08-15 17:56:30 -0500

    Contact page updated

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Form from '../components/Form'
 import Image from 'next/image'
-import logo from '../public/form.png'
+import logo from '../public/slider1.jpeg'
 import Head from 'next/head'
 import styles from '../styles/Contact.module.css'
+import JobForm from '@/components/JobForm'
 
 
 export default function Contact() {
 
   return (
     <>
-    <Head>
+      <Head>
         <title>Contact S&amp;W Foundation | Industrial Foundation Repair Experts </title>
         <meta name="description" content="S&amp;W Foundation provides high-quality commercial pier drilling services in the United States. Contact us today for a quote! Trust S&amp;W for and unmatched combo of expierience, equipment and safety" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -20,9 +21,9 @@ export default function Contact() {
         <meta property="og:url" content="https://www.swfoundation.com/contact/" />
         <meta property="og:image" content="https://www.swfoundation.com/images/form.png" />
         <meta property='og:site_name' content='S&amp;W Foundation Repair Contractors' />
-        <link rel="icon" href="/android-chrome-512x512.png" type='image/x-icon'/>
+        <link rel="icon" href="/android-chrome-512x512.png" type='image/x-icon' />
       </Head>
-    <div className={styles.contact}>
+      {/* <div className={styles.contact}>
       <div className={styles.container}>
         <div className={styles.wrapper}>
           <div className={styles.left}>
@@ -42,7 +43,37 @@ export default function Contact() {
         </div>
       </div>
       
-    </div>
+    </div> */}
+      <div className={styles.contact}>
+        <div className={styles.container}>
+          <div className={styles.wrapper}>
+            <div className={styles.left}>
+              <div className={styles.leftContainer}>
+                <div className={styles.leftWrapper}>
+                  <Image
+                    src={logo}
+                    className={styles.logo}
+                    alt='form logo'
+                    width={500}
+                    height={771}
+                    priority
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.right}>
+              <section className={styles.rightContainer}>
+                <section className={styles.top}>
+                  <Form />
+                </section>
+                <section className={styles.bottom}>
+                  <JobForm />
+                </section>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
