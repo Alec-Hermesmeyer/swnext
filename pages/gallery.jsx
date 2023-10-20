2e905bf1476e0ab68162e6e89f660072d477dddf
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-10-20 12:16:26 -0500

    Update gallery.jsx

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -7,20 +7,21 @@ import GalleryGrid from '@/components/GalleryGrid'
 export default function Gallery() {
     return (
         <>
-        <Head>
-    <title>Project Gallery | S&W Foundation - Dallas, TX's Premier Pier Drilling Specialists</title>
-    <meta name="description" content="Explore S&W Foundation's project gallery to witness our expertise in commercial pier drilling. From warehouses to hospitals, our Dallas, TX-based team delivers precision, safety, and excellence in every project." />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<Head>
     
-    <meta property="og:title" content="Project Gallery | S&W Foundation - Leading Pier Drilling Projects in Dallas, TX" />
-    <meta property="og:description" content="Dive into S&W Foundation's gallery, showcasing top-tier pier drilling projects across Dallas, TX and the wider US. Experience our dedication to quality firsthand." />
+    <title>Project Gallery | S&amp;W Foundation - Dallas, TX&apos;s Premier Pier Drilling Specialists</title>
+    <meta name="description" content="Explore S&amp;W Foundation&apos;s project gallery to witness our expertise in commercial pier drilling. From warehouses to hospitals, our Dallas, TX-based team delivers precision, safety, and excellence in every project." />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content="Project Gallery | S&amp;W Foundation - Leading Pier Drilling Projects in Dallas, TX" />
+    <meta property="og:description" content="Dive into S&amp;W Foundation&apos;s gallery, showcasing top-tier pier drilling projects across Dallas, TX and the wider US. Experience our dedication to quality firsthand." />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.swfoundation.com/gallery/" />
-{/*     <meta property="og:image" content="https://www.swfoundation.com/images/gallery-cover.jpg" /> <!-- This should be a representative image from the gallery --> */}
-    <meta property='og:site_name' content='S&W Commercial Pier Drilling' />
-    
+    {/* <meta property="og:image" content="https://www.swfoundation.com/images/gallery-cover.jpg" /> <!-- This should be a representative image from the gallery --> */}
+    <meta property='og:site_name' content='S&amp;W Commercial Pier Drilling' />
     <link rel="icon" href="/android-chrome-512x512.png" type='image/x-icon'/>
+    
 </Head>
+
 
       <div className={styles.main}>
         <GalleryGrid />
