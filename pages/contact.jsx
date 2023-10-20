6e114291752a2f5551f1a225c3cd9b37e5e0f4a8
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-10-20 12:41:54 -0500

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
     <title>Contact S&W Foundation | Commercial Pier Drilling Specialists in Dallas, TX</title>
     <meta name="description" content="Reach out to S&W Foundation, the leading specialists in commercial pier drilling across Dallas, TX and the wider US. Let's discuss how our expertise, advanced equipment, and commitment to safety can serve your project." />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    
+    <meta name="keywords" content="contact, commercial pier drilling, Dallas, TX, S&W Foundation, quote, project consultation, advanced equipment, safety, leading specialists, US"/>
     <meta property="og:title" content="Contact S&W Foundation | Commercial Pier Drilling Specialists in Dallas, TX" />
     <meta property="og:description" content="Looking for top-tier commercial pier drilling services in the US? Get in touch with S&W Foundation today for a comprehensive quote and experience our commitment to excellence." />
     <meta property="og:type" content="website" />
