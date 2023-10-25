e3be8bb2f10e38ea84717b3f14203bd55b44d11e
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-10-25 16:42:04 -0500

    Update gallery.jsx

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -18,6 +18,7 @@ export default function Gallery() {
     <meta property="og:url" content="https://www.swfoundation.com/gallery/" />
     <meta name="keywords" content="project gallery, S&amp;W Foundation, pier drilling specialists, Dallas, TX, commercial pier drilling, precision, safety, excellence, warehouses, hospitals, pier drilling projects, quality, dedication" />
     <meta property='og:site_name' content='S&amp;W Commercial Pier Drilling' />
+    <link rel="canonical" href="https://www.swfoundation.com/gallery" />
     <link rel="icon" href="/android-chrome-512x512.png" type='image/x-icon'/>
     
 </Head>
