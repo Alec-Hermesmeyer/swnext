e06d35aa9c8bc7b01b532a0f7804a6b406b1eaa8
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-11-15 16:07:21 -0600

    Update index.js
    GTM

--- pages/index.js
+++ pages/index.js
@@ -72,13 +72,11 @@ export default function Home({ data}) {
     <meta name="twitter:image" content="https://www.swfoundation.com/images/logo.png" />
     <link rel="canonical" href="https://www.swfoundation.com/" />
     <link rel="icon" href="/android-chrome-512x512.png" type='image/x-icon'/>
-    <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MJNDLQZ');</script>
-<!-- End Google Tag Manager -->
     <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(businessSchema) }} />
   
   
