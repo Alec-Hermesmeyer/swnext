4333eb4faffd205e4bb79bed5668d406e16502e0
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-11-15 16:18:07 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -105,10 +105,7 @@ export default function Home({ data}) {
       </Script>
 
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJNDLQZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
       <div className={styles.main}>
         <section style={{ marginBottom: 5 }} className={styles.hero}><Hero />
           
