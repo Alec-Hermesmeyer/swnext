165e35c5de7eed8245687b105b4244771472d5c8
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2023-11-15 15:46:54 -0600

    Update index.js
    GA4

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home({ data}) {
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-BXEC44GZQV"></script>
   <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)};
   gtag('js', new Date());
 
   gtag('config', 'G-BXEC44GZQV');
