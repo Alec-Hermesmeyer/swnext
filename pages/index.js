1b46f64b9d2c41128c21d14706bb30183200dd61
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2024-04-13 08:52:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import styles from "@/styles/Home.module.css";
 import { GridPattern } from "@/components/GridPattern";
 import { Container } from "@/components/Container";
