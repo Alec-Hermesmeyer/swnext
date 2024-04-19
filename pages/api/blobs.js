23c841f2ffeb497c643505a78926163275060ec1
Alec Hermessmeyer <alechermesmeyer@Alecs-MBP.lan>
2024-04-19 15:58:22 -0500

    fuck windows even harder

--- pages/api/blobs.js
+++ pages/api/blobs.js
@@ -1,12 +1,12 @@
-import { list } from '@vercel/blob';
-import { NextResponse } from 'next/server';
+// import { list } from '@vercel/blob';
+// import { NextResponse } from 'next/server';
  
-export const config = {
-  runtime: 'edge',
-};
+// export const config = {
+//   runtime: 'edge',
+// };
  
-export default async function blobs(request) {
-  const { blobs } = await list();
+// export default async function blobs(request) {
+//   const { blobs } = await list();
   
-  return NextResponse.json(blobs);
-}
+//   return NextResponse.json(blobs);
+// }
