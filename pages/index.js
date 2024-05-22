af2d506fec786ca2d3994bf0ebc6e53a6897a6c0
Alec Hermesmeyer <99496977+Alec-Hermesmeyer@users.noreply.github.com>
2024-05-22 13:55:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -66,12 +66,13 @@ function HeroSection() {
                     We Provide Nation-Wide Service
                   </h3>
                   <ul>
+                      <Link href="tel:2147030484">
                       <li className={lato.className}>
-                      <Link>
-                        <a href="tel:2147030484">Call: (214)-703-0484</a>
-                        </Link>
+                      
+                        Call: (214)-703-0484
+                        
                       </li>
-                 
+                 </Link>
                     <li>Address: 2806 Singleton St. Rowlett, TX 75088</li>
                   </ul>
                   <span>
