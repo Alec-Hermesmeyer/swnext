d2895434487cc0adc095a806ada83489d5ae8b98
Alec Hermessmeyer <alechermesmeyer@Alecs-MacBook-Pro.local>
2024-05-31 11:55:36 -0500

    node-cache and preloaded images

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import PreloadImages from '@/components/PreloadImages'
 import '@/styles/globals.css'
 
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
   }
   return (
     <Layout>
+      <PreloadImages />
       <Component {...pageProps} />
       <Analytics />
 
---
+++ pages/api/preload-images.js
@@ -0,0 +1,37 @@
+import { createClient } from '@supabase/supabase-js';
+import NodeCache from 'node-cache';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const cache = new NodeCache({ stdTTL: 3600 });
+
+export default async function handler(req, res) {
+  const cachedImages = cache.get('images');
+
+  if (cachedImages) {
+    console.log('Serving images from cache');
+    return res.status(200).json(cachedImages);
+  }
+
+  const folders = ['newimages', 'galleryImages']; // Subfolders inside 'public'
+  let images = [];
+
+  for (const folder of folders) {
+    const { data, error } = await supabase
+      .storage
+      .from('Images')
+      .list(`public/${folder}`, { limit: 100 });
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    images = [...images, ...data.map(file => ({ ...file, folder }))];
+  }
+
+  cache.set('images', images);
+
+  res.status(200).json(images);
+}
