2f075b6f4d0775e39bb81e0086957aa9baaa87d7
Alec Hermessmeyer <alechermesmeyer@Alecs-MacBook-Pro.local>
2024-06-14 12:55:39 -0500

    commit

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";import styles from '../styles/Admin.module.css';
-import withAuth from '@/components/withAuth';
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Admin.module.css";
+import withAuth from "@/components/withAuth";
 import { truncateText } from "@/utils/truncateText";
-import { GridPattern } from '@/components/GridPattern';
-import { useAuth } from '@/context/AuthContext';
+import { GridPattern } from "@/components/GridPattern";
+import { useAuth } from "@/context/AuthContext";
 import { createClient } from "@supabase/supabase-js";
 import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 
 const supabaseUrl = "https://edycymyofrowahspzzpg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeWN5bXlvZnJvd2Foc3B6enBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcyNTExMzAsImV4cCI6MTk5MjgyNzEzMH0.vJ8DvHPikZp2wQRXEbQ2h7JNgyJyDs0smEcJYjrjcVg";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-const inter = Inter({ subsets: ["latin"] });
-
-
+const lato = Lato({ weight: ["900"], subsets: ["latin"] });
 
 function Spacer() {
   return (
@@ -28,15 +28,14 @@ function ContactSubmissions() {
   const [totalPages, setTotalPages] = useState(0);
   const pageSize = 10; // Number of submissions per page
 
-
   useEffect(() => {
     const fetchTotalCount = async () => {
       const { data, error, count } = await supabase
-        .from('contact_form')
-        .select('*', { count: 'exact', head: true });
+        .from("contact_form")
+        .select("*", { count: "exact", head: true });
 
       if (error) {
-        console.error('Error fetching total count:', error);
+        console.error("Error fetching total count:", error);
       } else {
         setTotalPages(Math.ceil(count / pageSize));
       }
@@ -47,7 +46,10 @@ function ContactSubmissions() {
 
   useEffect(() => {
     const fetchContactSubmissions = async () => {
-      let { data, error } = await supabase.from("contact_form").select("*").range(page * pageSize, (page + 1) * pageSize - 1);
+      let { data, error } = await supabase
+        .from("contact_form")
+        .select("*")
+        .range(page * pageSize, (page + 1) * pageSize - 1);
       if (error) {
         console.log(error);
       } else {
@@ -71,11 +73,13 @@ function ContactSubmissions() {
   };
 
   const handleDelete = async (id) => {
-    const { error } = await supabase.from('contact_form').delete().eq('id', id);
+    const { error } = await supabase.from("contact_form").delete().eq("id", id);
     if (error) {
-      console.error('Error deleting contact submission:', error);
+      console.error("Error deleting contact submission:", error);
     } else {
-      setContactSubmission(contactSubmission.filter((submission) => submission.id !== id));
+      setContactSubmission(
+        contactSubmission.filter((submission) => submission.id !== id)
+      );
     }
   };
 
@@ -84,49 +88,99 @@ function ContactSubmissions() {
   }
 
   return (
+    // <div className={styles.contactSubContainer}>
+      // <table className={styles.contactSubTable}>
+      //   <thread className={styles.thread}>
+      //   <tr className={styles.tableRow}>
+      //       <th>Name</th>
+      //       <th>Email</th>
+      //       <th>Number</th>
+      //       <th>Message</th>
+      //       <th>Company</th>
+      //     </tr>
+      //   </thread>
+      //   <tbody className={styles.tableBody}>
+      //     {contactSubmission.map((submission) => (
+      //       <tr className={styles.tableRow2} key={submission.id}>
+      //         <td>{submission.name}</td>
+      //         <td>{submission.email}</td>
+      //         <td>{submission.number}</td>
+      //         <td title={submission.message}>
+      //         <div className={styles.tooltip}>
+      //             {truncateText(submission.message, 5)}
+      //             <span className={styles.tooltiptext}>{submission.message}</span>
+      //           </div>
+      //         </td>
+      //         <td>{submission.company}</td>
+      //         <span className={styles.deleteBtnContainerT}>
+      //         <button className={styles.deleteBtn} onClick={() => handleDelete(submission.id)}>Delete</button>
+      //       </span>
+      //       </tr>
+      //     ))}
+      //   </tbody>
+      // </table>
+    //   <div className={styles.pagination}>
+    //     <button onClick={handlePreviousPage} disabled={page === 0}>
+    //       Previous
+    //     </button>
+    //     <span>Page {page + 1} of {totalPages}</span>
+    //     <button onClick={handleNextPage} disabled={page >= totalPages - 1}>
+    //       Next
+    //     </button>
+    //   </div>
+    // </div>
     <div className={styles.contactSubContainer}>
-      <table className={styles.contactSubTable}>
+      <h1>Contact Submissions</h1>
+      <div className={styles.contactSubWrapper}>
+        {contactSubmission.map((submission) => (
+          <div className={styles.contactSubCard} key={submission.id}>
+            <details className={styles.details}>
+              <summary className={lato.className}> {submission.name}</summary>
+              <table className={styles.contactSubTable}>
         <thread className={styles.thread}>
         <tr className={styles.tableRow}>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Number</th>
-            <th>Message</th>
-            <th>Company</th>
+            <th className={lato.className}>Name</th>
+            <th className={lato.className}>Email</th>
+            <th className={lato.className}>Number</th>
+            <th className={lato.className}>Message</th>
+            <th className={lato.className}>Company</th>
           </tr>
         </thread>
         <tbody className={styles.tableBody}>
-          {contactSubmission.map((submission) => (
-            <tr className={styles.tableRow2} key={submission.id}>
-              <td>{submission.name}</td>
-              <td>{submission.email}</td>
-              <td>{submission.number}</td>
-              <td title={submission.message}>
+            <tr className={styles.tableRow2}>
+              <td className={lato.className}>{submission.name}</td>
+              <td className={lato.className}>{submission.email}</td>
+              <td className={lato.className}>{submission.number}</td>
+              <td className={lato.className} title={submission.message}>
               <div className={styles.tooltip}>
                   {truncateText(submission.message, 5)}
                   <span className={styles.tooltiptext}>{submission.message}</span>
                 </div>
               </td>
-              <td>{submission.company}</td>
+              <td className={lato.className}>{submission.company}</td>
               <span className={styles.deleteBtnContainerT}>
               <button className={styles.deleteBtn} onClick={() => handleDelete(submission.id)}>Delete</button>
             </span>
             </tr>
-          ))}
         </tbody>
       </table>
-      <div className={styles.pagination}>
-        <button onClick={handlePreviousPage} disabled={page === 0}>
-          Previous
-        </button>
-        <span>Page {page + 1} of {totalPages}</span>
-        <button onClick={handleNextPage} disabled={page >= totalPages - 1}>
-          Next
-        </button>
+            </details>
+          </div>
+        ))}
+        <div className={styles.pagination}>
+          <button onClick={handlePreviousPage} disabled={page === 0}>
+            Previous
+          </button>
+          <span>
+            Page {page + 1} of {totalPages}
+          </span>
+          <button onClick={handleNextPage} disabled={page >= totalPages - 1}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
-
 }
 function JobApplicants() {
   //this function will display job applicants from the supabase database, allowing admin to view and delete them
@@ -136,15 +190,14 @@ function JobApplicants() {
   const [totalPages, setTotalPages] = useState(0);
   const pageSize = 10; // Number of submissions per page
 
-
   useEffect(() => {
     const fetchTotalCount = async () => {
       const { data, error, count } = await supabase
-        .from('job_form')
-        .select('*', { count: 'exact', head: true });
+        .from("job_form")
+        .select("*", { count: "exact", head: true });
 
       if (error) {
-        console.error('Error fetching total count:', error);
+        console.error("Error fetching total count:", error);
       } else {
         setTotalPages(Math.ceil(count / pageSize));
       }
@@ -155,7 +208,10 @@ function JobApplicants() {
 
   useEffect(() => {
     const fetchJobSubmissions = async () => {
-      let { data, error } = await supabase.from("job_form").select("*").range(page * pageSize, (page + 1) * pageSize - 1);
+      let { data, error } = await supabase
+        .from("job_form")
+        .select("*")
+        .range(page * pageSize, (page + 1) * pageSize - 1);
       if (error) {
         console.log(error);
       } else {
@@ -179,11 +235,13 @@ function JobApplicants() {
   };
 
   const handleDelete = async (id) => {
-    const { error } = await supabase.from('job_form').delete().eq('id', id);
+    const { error } = await supabase.from("job_form").delete().eq("id", id);
     if (error) {
-      console.error('Error deleting contact submission:', error);
+      console.error("Error deleting contact submission:", error);
     } else {
-      setJobSubmission(jobSubmission.filter((submission) => submission.id !== id));
+      setJobSubmission(
+        jobSubmission.filter((submission) => submission.id !== id)
+      );
     }
   };
 
@@ -193,44 +251,54 @@ function JobApplicants() {
 
   return (
     <div className={styles.contactSubContainer}>
-      <table className={styles.contactSubTable}>
+      <h1 className={lato.className}>Job Applicants</h1>
+      <div className={styles.contactSubWrapper}>
+        {jobSubmission.map((submission) => (
+          <div className={styles.contactSubCard} key={submission.id}>
+            <details className={styles.details}>
+              <summary className={lato.className}> {submission.name}</summary>
+              <table className={styles.contactSubTable}>
         <thread className={styles.thread}>
         <tr className={styles.tableRow}>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Number</th>
-            <th>Message</th>
-            <th>Position</th>
+            <th className={lato.className}>Name</th>
+            <th className={lato.className}>Email</th>
+            <th className={lato.className}>Number</th>
+            <th className={lato.className}>Message</th>
+            <th className={lato.className}>Position</th>
           </tr>
         </thread>
         <tbody className={styles.tableBody}>
-          {jobSubmission.map((submission) => (
-            <tr className={styles.tableRow2} key={submission.id}>
-              <td>{submission.name}</td>
-              <td>{submission.email}</td>
-              <td>{submission.number}</td>
-              <td title={submission.message}>
+            <tr className={styles.tableRow2}>
+              <td className={lato.className}>{submission.name}</td>
+              <td className={lato.className}>{submission.email}</td>
+              <td className={lato.className}>{submission.number}</td>
+              <td className={lato.className} title={submission.message}>
               <div className={styles.tooltip}>
                   {truncateText(submission.message, 5)}
                   <span className={styles.tooltiptext}>{submission.message}</span>
                 </div>
               </td>
-              <td>{submission.position}</td>
-              <span className={styles.deleteBtnContainer}>
+              <td className={lato.className}>{submission.position}</td>
+              <span className={styles.deleteBtnContainerT}>
               <button className={styles.deleteBtn} onClick={() => handleDelete(submission.id)}>Delete</button>
             </span>
             </tr>
-          ))}
         </tbody>
       </table>
-      <div className={styles.pagination}>
-        <button onClick={handlePreviousPage} disabled={page === 0}>
-          Previous
-        </button>
-        <span>Page {page + 1} of {totalPages}</span>
-        <button onClick={handleNextPage} disabled={page >= totalPages - 1}>
-          Next
-        </button>
+            </details>
+          </div>
+        ))}
+        <div className={styles.pagination}>
+          <button onClick={handlePreviousPage} disabled={page === 0}>
+            Previous
+          </button>
+          <span>
+            Page {page + 1} of {totalPages}
+          </span>
+          <button onClick={handleNextPage} disabled={page >= totalPages - 1}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -243,7 +311,6 @@ function AddUser() {
       <h1>Add User</h1>
     </div>
   );
-
 }
 
 function DeleteUser() {
@@ -253,15 +320,14 @@ function DeleteUser() {
       <h1>Delete User</h1>
     </div>
   );
-
-
 }
 function Logout() {
   const { logout } = useAuth();
   return (
-    <button className={styles.logoutBtn} onClick={logout}>Logout</button>
+    <button className={styles.logoutBtn} onClick={logout}>
+      Logout
+    </button>
   );
-
 }
 const Admin = () => {
   const { logout } = useAuth();
@@ -275,10 +341,10 @@ const Admin = () => {
       <section className={styles.contactWidget}>
         <JobApplicants />
       </section>
-      <Logout className={styles.lowerBtn}/>
+      <Logout className={styles.lowerBtn} />
       <Spacer className={styles.spacer} />
     </div>
-  )
-}
+  );
+};
 
-export default withAuth(Admin)
+export default withAuth(Admin);
