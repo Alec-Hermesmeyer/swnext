e8131b2cb7ca0b80a0c21b284a1cac0c4022a6f2
Alec Hermessmeyer <alechermesmeyer@Alecs-MacBook-Pro.local>
2024-07-19 12:25:47 -0500

    commit

--- components/Supabase.js
+++ components/Supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = NEXT_PUBLIC_SUPABASE_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
