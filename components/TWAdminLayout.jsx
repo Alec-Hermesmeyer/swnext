327ddeab7edc7622165044123f1950be6aa81754
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2026-04-07 16:57:09 -0500

    🔖 Checkpoint: Edit /Users/alechermesmeyer/Desktop/SWF/final/swnext/components/TWAdminLayout.jsx

--- components/TWAdminLayout.jsx
+++ components/TWAdminLayout.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useRef, useEffect } from "react";
 import { Lato } from "next/font/google";
 import AIChatBubble from "@/components/admin/AIChatBubble";
 import { useAuth } from "@/context/AuthContext";
