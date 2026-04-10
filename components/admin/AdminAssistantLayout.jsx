37985e1423f5cab813597c0f0745aa326bd711ce
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2026-04-10 14:13:25 -0500

    commit

--- components/admin/AdminAssistantLayout.jsx
+++ components/admin/AdminAssistantLayout.jsx
@@ -21,7 +21,7 @@ export default function AdminAssistantLayout({ children }) {
         <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.22),transparent_54%),linear-gradient(180deg,rgba(255,255,255,0.08),rgba(255,255,255,0.02))]" />
       </div>
 
-      <div className="relative z-10 mx-auto flex h-full min-h-0 w-full max-w-[1500px] flex-1 flex-col px-4 py-4 md:px-6 md:py-6">
+      <div className="relative z-10 mx-auto flex h-full min-h-0 w-full max-w-[1650px] flex-1 flex-col px-4 py-4 md:px-6 md:py-6 xl:px-8">
         {children}
       </div>
     </div>
