1db723f6f501704112caabffcad2ea64c711ad41
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2026-04-20 08:01:18 -0500

    🔖 Checkpoint: Edit /Users/alechermesmeyer/Desktop/SWF/frontend/swnext/components/admin/HiringPipeline.jsx

--- components/admin/HiringPipeline.jsx
+++ components/admin/HiringPipeline.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { HIRING_PIPELINE_STAGES, hiringStageLabel } from "@/lib/hiring-pipeline";
 
