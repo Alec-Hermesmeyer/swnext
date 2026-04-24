c4be960616c08fde8c99be2dfdbda52be0f34e57
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2026-04-24 13:19:32 -0500

    🔖 Checkpoint: Edit /Users/alechermesmeyer/Desktop/SWF/frontend/swnext/components/admin/client-portal/index.js

---
+++ components/admin/client-portal/index.js
@@ -0,0 +1,6 @@
+export { default as PortalRow } from "./PortalRow";
+export { default as PortalFormModal } from "./PortalFormModal";
+export { default as PortalDocsDrawer } from "./PortalDocsDrawer";
+export { default as PortalJobsDrawer } from "./PortalJobsDrawer";
+export * from "./constants";
+export * from "./helpers";
